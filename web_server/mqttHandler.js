fa5751ef8547da4e0e92611b5b9b66a4a1d147d4
Jung Inho <area1211@naver.com>
2019-04-19 22:58:26 +0900

    file path 변경

--- web_server/mqttHandler.js
+++ web_server/mqttHandler.js
@@ -3,10 +3,10 @@ const fs = require('fs');
 
 // var imageList = new Array(901);
 var imageList = [];
-for(var n=1; n<901; n++){
-    var tempUrl0 = "./public/images/mqtt_900/image_part_" + n + ".jpg";
-    var tempUrl1 = "./public/images/mqtt_900/image_part_0" + n + ".jpg";
-    var tempUrl2 = "./public/images/mqtt_900/image_part_00" + n + ".jpg";
+for (var n = 1; n <= 100; n++) {
+    // var tempUrl0 = "./public/images/mqtt_900/image_part_" + n + ".jpg";
+    // var tempUrl1 = "./public/images/mqtt_900/image_part_0" + n + ".jpg";
+    // var tempUrl2 = "./public/images/mqtt_900/image_part_00" + n + ".jpg";
     // if (n < 10)
     //     // tempUrl += "00";
     //     tempUrl.concat("00");
@@ -17,13 +17,13 @@ for(var n=1; n<901; n++){
     // tempUrl = tempUrl + n + ".jpg";
     // console.log(tempUrl);
     // console.log(typeof tempUrl);
-    var url;
-    if (n < 10)
-        url = tempUrl2;
-    else if (n >= 10 && n < 100)
-        url = tempUrl1;
-    else
-        url = tempUrl0;
+    var url = "./public/images/400B/50x50/blue" + n + ".jpg";
+    // if (n < 10)
+    //     url = tempUrl2;
+    // else if (n >= 10 && n < 100)
+    //     url = tempUrl1;
+    // else
+    //     url = tempUrl0;
 
 
     // var url = "./public/images/mqtt/" + n + ".jpg";
@@ -31,7 +31,7 @@ for(var n=1; n<901; n++){
 }
 
 class MqttHandler {
-    constructor(portNum){        
+    constructor(portNum) {
         var settings = {
             port: portNum,
         };
@@ -39,22 +39,22 @@ class MqttHandler {
         this.server = new mosca.Server(settings);
     }
 
-    mqttConnect(){
+    mqttConnect() {
         var mqttServer = this.server;
 
-        this.server.on('clientConnected', function(client) {
+        this.server.on('clientConnected', function (client) {
             console.log('client connected', client.id);
         });
-    
+
         // fired when a message is received
-        this.server.on('published', function(packet, client) {
-            if(packet.topic == "mytopic" && client != null) {
+        this.server.on('published', function (packet, client) {
+            if (packet.topic == "mytopic" && client != null) {
                 imageList.forEach(function (item, index, array) {
                     console.log("index : ", index, item);
-                    fs.readFile(item, function(error, data){
+                    fs.readFile(item, function (error, data) {
                         console.log(typeof data);
                         var sendData = {
-                            'number': index+1,
+                            'number': index + 1,
                             'image': data.toString('base64')
                         }
                         console.log('sendData', sendData);
@@ -69,14 +69,14 @@ class MqttHandler {
 
                         console.log('newPacket', newPacket);
 
-                        mqttServer.publish(newPacket, function() {
+                        mqttServer.publish(newPacket, function () {
                             console.log('done!');
                         });
                     });
                 });
             }
         });
-    
+
         this.server.on('ready', setup);
 
         // fired when the mqtt server is ready
